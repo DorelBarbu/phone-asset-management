494b1636e10f5701898d9c3b2cccc3dfeecb6485
Dorel Barbu <dobarbu@bitdefender.biz>
2022-01-11 19:18:42 +0200

    Wire phone list to phone list container

--- src/app/phone-list-container/phone-list-container/phone-list-container.component.ts
+++ src/app/phone-list-container/phone-list-container/phone-list-container.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import phones from 'src/mocks/phones';
+import Phone from '../../../types/phone';
 @Component({
   selector: 'app-phone-list-container',
   templateUrl: './phone-list-container.component.html',
   styleUrls: ['./phone-list-container.component.css']
 })
 export class PhoneListContainerComponent implements OnInit {
+
+  phones: Phone[] = phones;
 
   constructor() { }
 
